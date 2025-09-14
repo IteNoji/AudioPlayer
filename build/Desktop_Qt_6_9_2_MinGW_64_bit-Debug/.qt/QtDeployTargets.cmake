123b45045b181007fe3754ba21d8e4df3284f555
IteNoji <58850347+IteNoji@users.noreply.github.com>
2025-09-14 20:49:16 +0800

    Delete build directory

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AudioPlayer_FILE D:/Development/QT/project/AudioPlayer/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/AudioPlayer.exe)
-set(__QT_DEPLOY_TARGET_AudioPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AudioPlayer_RUNTIME_DLLS D:/Development/QT/6.9.2/mingw_64/bin/Qt6Multimedia.dll;D:/Development/QT/6.9.2/mingw_64/bin/Qt6Widgets.dll;D:/Development/QT/6.9.2/mingw_64/bin/Qt6Gui.dll;D:/Development/QT/6.9.2/mingw_64/bin/Qt6Network.dll;D:/Development/QT/6.9.2/mingw_64/bin/Qt6Core.dll)
